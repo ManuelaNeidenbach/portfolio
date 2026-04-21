24e49a49ccc1cc01d37c8816e32f1fad1f915f4c
Manuela Neidenbach <m.neidenbach686@web.de>
2026-04-21 10:29:54 +0200

    neue anordnung

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import './App.css'
+import Header from './Header'
+import ProfileName from './ProfileName'
+import ProfileImage from './ProfileImage'
+import AboutText from './AboutText'
+import HobbySection from './HobbySection'
+import PagePreviewCard from './PagePreviewCard'
 
 function App() {
   return (
-    <main className="app">
-      <h1>Meine Website</h1>
-      <p>Hier entsteht meine eigene React-Website.</p>
-    </main>
+    <div className="page">
+      <Header />
+
+      <main className="about-page">
+        <section className="intro-section">
+          <ProfileName />
+          <div className="intro-content">
+            <AboutText />
+            <ProfileImage />
+          </div>
+        </section>
+
+        <HobbySection />
+
+        <section className="preview-section">
+          <PagePreviewCard
+            title="Meine Auswanderung"
+            text="Hier erzähle ich von unserem Weg nach Zypern, unseren Gründen und unseren Plänen."
+          />
+          <PagePreviewCard
+            title="Kontakt"
+            text="Hier kannst du mich erreichen und mehr über meine Projekte erfahren."
+          />
+        </section>
+      </main>
+    </div>
   )
 }
 
